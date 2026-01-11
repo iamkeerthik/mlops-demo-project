be306cae71634cdec0e374986ec6e7e9ccc2424d
Keerthik Shenoy <keerthikshenoy@Keerthiks-MacBook-Air.local>
2026-01-11 09:06:22 +0530

    adding infra pipeline

---
+++ terraform/01-bootstrap/ecr.tf
@@ -0,0 +1,17 @@
+resource "aws_ecr_repository" "app" {
+  name                 = var.ecr_repo_name
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Environment = var.env
+    ManagedBy   = "terraform"
+  }
+}
