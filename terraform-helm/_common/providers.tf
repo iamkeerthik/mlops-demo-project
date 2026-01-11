8182d30243ccdb25036358504f6787bdac2998b3
Keerthik Shenoy <keerthikshenoy@Keerthiks-MacBook-Air.local>
2026-01-11 12:24:08 +0530

    adding infra pipeline

--- terraform-helm/_common/providers.tf
+++ terraform-helm/_common/providers.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = data.aws_eks_cluster.mlops_cluster.endpoint
     cluster_ca_certificate = base64decode(
-      data.aws_eks_cluster.this.certificate_authority[0].data
+      data.aws_eks_cluster.mlops_cluster.certificate_authority[0].data
     )
     token = data.aws_eks_cluster_auth.cluster_auth.token
   }
